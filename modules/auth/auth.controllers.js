64c4d0f3d538436fb2dcb001e5ee70d3cbc4b713
Sergy Yakovenko <sergeyyakovenko15@gmail.com>
2025-10-19 20:44:33 +0500

    Удалены лишние .js файлы

--- modules/auth/auth.controllers.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var AuthControllers = /** @class */ (function () {
-    function AuthControllers() {
-    }
-    return AuthControllers;
-}());
-exports.default = AuthControllers;
