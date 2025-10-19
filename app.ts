02af91bf738b787561f1fb741c3e9bead0c0fa2d
Sergy Yakovenko <sergeyyakovenko15@gmail.com>
2025-10-19 21:58:08 +0500

    Удалаены лишние .js файлы, изменино описание проекта, исправлен README.md файл

--- app.ts
+++ app.ts
@@ -9,11 +9,6 @@ import UserRepository from "./modules/user/user.repo.ts";
 import TokenRepository from "./modules/token/token.repo.ts";
 import UserService from "./modules/user/user.service.ts";
 import TokenService from "./modules/token/token.service.ts";
-// import FileRepository from "./modules/files/file.repo.ts";
-// import FileService from "./modules/files/file.service.ts";
-// import FileRoutes from "./modules/files/file.routes.ts";
-// import { isAuthenticated } from "./middlewars/auth.middleware.ts";
-// import FileUploader from "./libs/file-uploader.ts";
 import { errorMiddleware } from "./middlewars/error.middleware.ts";
 import { AuthRouter } from "./routes/auth.ts";
 import { setTokenServiceMiddleware } from "./middlewars/set-token-service.middleware.ts";
@@ -32,27 +27,16 @@ app.set("trust proxy", 1);
 
 const uploadsPath = "uploads";
 app.use(`/${uploadsPath}`, express.static(uploadsPath));
-// const fileUploader = new FileUploader({ path: uploadsPath });
-//
+
 const userRepo = new UserRepository(db);
 const tokenRepo = new TokenRepository(db);
-// const fileRepo = new FileRepository(db);
-//
+
 const userService = new UserService({ userRepository: userRepo });
 const tokenService = new TokenService({ tokenRepository: tokenRepo });
-// const fileService = new FileService({ fileRepository: fileRepo, fileUploader });
-//
 const authService = new AuthService({ userService, tokenService });
 
 app.use(setTokenServiceMiddleware(tokenService));
 app.use(setFingerprintMiddleware);
-
-// app.use(
-//   "/file",
-//   isAuthenticated,
-//   new FileRoutes({ fileService, fileUploader })
-// );
-
 app.use(AuthRouter({ authController: new AuthControllers({ authService }) }));
 app.use(errorMiddleware);
 
